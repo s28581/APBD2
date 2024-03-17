8729a2d7ab8c5fa859a6606b4e8fecdae75d75ec
s28581 <s28581@pjwstk.edu.pl>
2024-03-17 23:51:42 +0100

    AverageOfArray

--- APBD2/APBD2/Program.cs
+++ APBD2/APBD2/Program.cs
@@ -4,3 +4,14 @@ Console.WriteLine("Hello, World!");
 Console.WriteLine("1");
 Console.WriteLine("2");
 Console.WriteLine("3");
+
+//zad1
+static int AverageOfArray(int[] arr)
+{
+    int sum = 0;
+    foreach (int num in arr)
+    {
+        sum += num;
+    }
+    return sum / arr.Length;
+}
